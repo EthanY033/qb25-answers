3fb0ef64fec8efa2eef3abba47218ef5cc1a0921
EthanY033 <ethanyy033@gmail.com>
2025-10-31 02:10:27 -0400

    exercise 1

---
+++ week7/README.md
@@ -0,0 +1,19 @@
+**Questions in Section 1.3**     
+1. Does everything look okay?    
+   
+Most parts of the PCA plot looks good, but the Fe and LFC.Fe groups are mixed together, and 3 replicates of A2.3 don't cluster very well.    
+    
+Fixation: apply z-score to the data.    
+   
+
+2. What does the PCA plot suggest to you about the data? Why?   
+   
+The PCA data shows that the replicates of the same sample are close to each other. This is obvious since results of biological replicates should be similar to each other.   
+   
+
+3. What feature explains the first principal component (simply saying “tissue” is not sufficient)?   
+   
+The heatmap and PC1 plot show that the PC1 increases from A1 to P2.4, with similar values for each replicate. Therefore, PC1 is best explained by tissue and less explained by replicate number.   
+   
+
+
---
+++ week7/R_script.R
@@ -0,0 +1,69 @@
+library(tidyr)
+library(dplyr)
+library(matrixStats)
+library(ggplot2)
+
+
+####### Step 1.1 #######
+data_rna <- as.matrix(read.table("/Users/cmdb/qb25-answers/week7/read_matrix.tsv"))
+
+data_rna <- data_rna %>% 
+  cbind(rowSds(data_rna))
+
+colnames(data_rna)[22] <- "Sd"
+data_rna <- data_rna[order(data_rna[, "Sd"], decreasing = TRUE), ]
+data_rna_sd500 <- data_rna[1:500, ]
+
+####### Step 1.2 #######
+data_rna_sd500 <- t(data_rna_sd500[, -22])
+
+pca_results = prcomp(data_rna_sd500)
+PC_12 <- data.frame(pca_results$x[, 1:2])
+PC_12$legend <- rownames(PC_12)
+PC_12 <- PC_12 %>%
+  separate(legend, into = c("Tissue", "Replicate"), sep = "_")
+
+PC_12 %>% 
+  ggplot(aes(PC1, PC2, color=Tissue, shape=Replicate)) +
+  geom_point() +
+  theme_classic()
+ggsave("/Users/cmdb/qb25-answers/week7/PCA_plot.png")
+
+
+
+data_rna_sd500_norm <- scale(data_rna_sd500)
+
+pca_results_norm = prcomp(data_rna_sd500_norm)
+PC_12_norm <- data.frame(pca_results_norm$x[, 1:2])
+PC_12_norm$legend <- rownames(PC_12_norm)
+PC_12_norm <- PC_12_norm %>%
+  separate(legend, into = c("Tissue", "Replicate"), sep = "_")
+
+PC_12_norm %>% 
+  ggplot(aes(PC1, PC2, color=Tissue, shape=Replicate)) +
+  geom_point() +
+  theme_classic()
+ggsave("/Users/cmdb/qb25-answers/week7/PCA_plot_normalized.png")
+
+png("pca_heatmap_x.png")
+heatmap(as.matrix(pca_results_norm$x), Rowv = NA, Colv = NA)
+dev.off()
+
+png("pca_heatmap_rotation.png", width = 800, height = 600)
+heatmap(as.matrix(pca_results_norm$rotation), Rowv = NA, Colv = NA)
+dev.off()
+
+var <- pca_results_norm$sdev^2
+var_data <- data.frame(
+  PC = c(1: 21),
+  var = var
+)
+
+ggplot(var_data, aes(x = PC, y = var)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Variance Explained by Principal Components",
+       x = "Number of Principal Components",
+       y = "Variance Explained") +
+  theme_minimal()
+ggsave("/Users/cmdb/qb25-answers/week7/variance_explained.png")
+
